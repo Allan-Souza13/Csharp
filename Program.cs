c98a11f169888ca891106ec39bdeafa586de2b50
Allan Pereira de Souza <77082266+Allan-Souza13@users.noreply.github.com>
2023-02-02 15:25:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 10;
+            Console.WriteLine(idade);   
+
+            idade= 10 + 5;
+            Console.WriteLine(idade);       
+
+            idade= (10 + 5)*2;
+            Console.WriteLine(idade);
+            
+            Console.WriteLine("Sua Idade é: " + idade);
+            
+
+
+            Console.WriteLine("Execução Finalizada! Tecle enter para sair. . .");
+            Console.ReadLine();
+        }
+        
+    }
+}
